44ef1c4aaa094d03b100c47e8d65313bb26a91a7
Anish Shah <ahshah@stu.naperville203.org>
2025-11-11 17:18:26 -0600

    started shepard fairy and finished color labs

---
+++ ColorLabs17/colorlabs25/testpic.java
@@ -0,0 +1,222 @@
+
+/**
+ * Kevin Hayes
+ * Test Picture Classes
+ *
+ * @author (Kevin Hayes)
+ * @version (10-19-2016)
+ */
+import java.awt.*;
+import java.util.*;
+import java.util.List; // resolves problem with java.awt.List and java.util.List
+public class testpic
+{
+
+    /**
+     * main method, to test the picture
+     *
+     */
+  public static void main(String[] args)
+  {
+      //opens picture using a dialog box
+      /*
+     String fileName = FileChooser.pickAFile();
+     Picture pictObj = new Picture(fileName);
+     pictObj.explore(); */
+
+     //opens a pictue using a path
+     //Picture apic = new Picture("C:\\Users\\khayes\\Favorites\\Documents\APCS- Java\chap03\Curriclum 2013\Picture Color labs\images\\beach.jpg");
+     
+     //relative path
+     //KNOW THIS - ON TEST
+     //                          folder/file
+     Picture apic = new Picture("images\\beach.jpg");
+     Picture ferris1 = new Picture("images/2000 ferris wheel2.jpg");
+     Picture moto = new Picture("images/redMotorcycle.jpg");
+     Picture ferris3 = new Picture("images/2000 ferris wheel2.jpg");
+     Picture shrine = new Picture("images/femaleLionAndHall.jpg");
+     
+     
+
+     //apic.explore();
+     moto.explore();
+     ferris1.explore();
+     //displays the picture
+     
+     //makes an array of pixels
+     //NOT ON TEST - arrays specfically
+     Pixel[] pixels;
+     //gets pixels from picture and assigns to pixels array
+     pixels = ferris1.getPixels();
+     //gets ALL pixles
+     
+     Pixel[] mpixels;
+     mpixels = moto.getPixels();
+     
+     Pixel [] spixels;
+     spixels = shrine.getPixels();
+    
+     //how many pixels or how large array
+    System.out.println("This is a large array"+pixels.length  );
+
+
+    /**/
+        //access each index
+    System.out.println(pixels[17]);
+    //access each pixel
+    // you can use get pixel to get a pixel at a specific spot
+    Pixel spot = ferris1.getPixel(100,100);
+    Pixel spot2 = ferris1.getPixel(433,281);
+    Pixel ferr17 = pixels[17];
+    
+    ferr17.setRed(240);
+    ferr17.setGreen(160);
+    ferr17.setBlue(200);
+    
+    Color newColor = new Color(70,130,180);
+    
+    spot2.setColor(Color.blue);
+    spot.setColor(newColor);
+    
+    
+    ferris1.explore();
+    //you have to explore it to see changes
+    
+    //all 0 is black
+    //all 255 is white
+    System.out.println(pixels[17].getColor());
+    System.out.println(spot);
+    
+    for(int i = 0; i<10000; i++) {
+        Pixel yuck  = ferris1.getPixel((int)(Math.random()*500), (int)(Math.random()*500));
+        yuck.setColor(newColor);
+    }
+    ferris1.explore();
+    
+ /*
+    pixels[17].setColor(Color.blue);
+    spot.setColor(new Color(252,252,252));
+    pixels[500034].setColor(Color.blue);
+
+    ferris1.explore();
+/**/
+   // loop to access indexes of array or collection
+
+    //for each loop spot  is a ?
+    
+    
+    
+    
+    
+    
+    //
+    /*
+    int red;
+    int blue;
+    int green;
+    for (Pixel spot1: mpixels){
+        //System.out.println( spot );
+        red = spot1.getRed();
+        
+        red = (int)(red * .25);
+        
+        spot1.setRed(red);
+        
+    }
+    
+    
+    for (Pixel spot3: mpixels){
+        blue = spot3.getBlue();
+        
+        blue = (int)(blue * Math.random());
+        
+        spot3.setBlue(blue);
+    }
+
+    for (Pixel spot4: mpixels){
+        green = spot4.getGreen();
+        
+        green = (int)(green * Math.random());
+        
+        spot4.setGreen(green);
+    }
+    moto.explore();
+    /**/
+    
+    
+    //Lab 1
+    //adjusting Red, Green and Blue
+    int reds;
+    int blues;
+    int greens;
+    for (Pixel spot5: spixels){
+        //System.out.println( spot );
+        reds = spot5.getRed();
+        blues = spot5.getBlue();
+        greens = spot5.getGreen();
+        
+        reds = (int)(reds * .5);
+        blues = (int)(blues * .25);
+        greens = (int)(greens * 1.5);
+        
+        spot5.setRed(reds);
+        spot5.setBlue(blues);
+        spot5.setGreen(greens);
+        
+        
+    }
+    shrine.explore();
+    
+    //
+    
+ /**/
+
+ /**
+  * Method to clear red from picture
+  * @param none
+  * @return none
+  */
+ /*
+    for (Pixel pixelObj : pixels)
+        {
+            //set the red value of the current pixel to the new value
+           
+
+        }
+    ferris1.explore();
+    
+/**/
+ /**
+  * Method to reduce red from picture by a factor of n
+  * @param none
+  * @return none
+  */
+
+/*
+int value;
+final double  FACTOR = .5;
+    for (Pixel pixelObj : pixels)
+    {
+
+        //get the redvalue
+        value = pixelObj.getRed();
+        //System.out.println(value);
+
+        //decrease the red value by 50%
+        
+        //set the red value of the current pixel to the new value
+        
+
+    }
+    // use new picture when changing or it will make changes to 
+    // pic you already changed
+    ferris1.explore();
+    ferris2.explore();
+
+  /**/ 
+    //write/save a picture as a file
+    ferris1.write("images/ferris11.jpg");
+
+    /**/
+  }//main
+}//class
